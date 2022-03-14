b3af542e44d44e76ea56f5c5829346d4df4ac593
Rye-Catcher <2427367956@qq.com>
2022-03-15 01:57:44 +0800

    Implement drag and move features in StrategyPanel

---
+++ src/main/java/seedu/address/ui/StrategyPanel.java
@@ -0,0 +1,73 @@
+package seedu.address.ui;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class StrategyPanel extends UiPart<Region> {
+    private static final String FXML = "StrategyPanel.fxml";
+
+    private static double orgSceneX;
+    private static double orgSceneY;
+    private static double orgTranslateX;
+    private static double orgTranslateY;
+
+    @FXML
+    private Circle player1;
+    @FXML
+    private Circle player2;
+    @FXML
+    private Circle player3;
+
+    private EventHandler<MouseEvent> pressHandler =
+            new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                orgSceneX = t.getSceneX();
+                orgSceneY = t.getSceneY();
+                orgTranslateX = ((Circle) (t.getSource())).getTranslateX();
+                orgTranslateY = ((Circle) (t.getSource())).getTranslateY();
+            }
+    };
+
+    private EventHandler<MouseEvent> dragHandler =
+            new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                double offsetX = t.getSceneX() - orgSceneX;
+                double offsetY = t.getSceneY() - orgSceneY;
+                double newTranslateX = orgTranslateX + offsetX;
+                double newTranslateY = orgTranslateY + offsetY;
+                Circle tmp = (Circle) (t.getSource());
+                tmp.setTranslateX(newTranslateX);
+                tmp.setTranslateY(newTranslateY);
+            }
+    };
+
+    /**
+     * Creates a {@code StrategyPanel} with draggable circles.
+     */
+    public StrategyPanel() {
+        super(FXML);
+        initCircle(player1, 50, 100, 100, Color.RED);
+        initCircle(player2, 50, 200, 200, Color.LIGHTBLUE);
+        initCircle(player3, 30, 200, 200, Color.YELLOW);
+    }
+
+    private void initCircle(Circle circle, double rad, double x, double y, Paint color) {
+        circle.setRadius(rad);
+        circle.setFill(color);
+        circle.setCenterX(x);
+        circle.setCenterY(y);
+        circle.setCursor(Cursor.HAND);
+        circle.setOnMousePressed(pressHandler);
+        circle.setOnMouseDragged(dragHandler);
+    }
+}
