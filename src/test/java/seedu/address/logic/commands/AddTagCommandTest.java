3fa34c8d2d2c62e8d2e7efc9b66baf01c946c294
juayhee <e0524722@u.nus.edu>
2022-03-17 17:36:49 +0800

    Fix import checkstyle

--- src/test/java/seedu/address/logic/commands/AddTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTagCommandTest.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
