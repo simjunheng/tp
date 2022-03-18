1fd20badc34c96326cf6cebfe11e524bf3ae878d
AAlghrairy <abdulelah.faisal@gmail.com>
2022-03-18 17:11:35 +0800

    Add test cases for DeleteNoteCommandParser related classes

---
+++ src/test/java/seedu/address/logic/parser/DeleteMiscCommandParserTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.NOTE_FIRST_INDEX;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeleteMiscCommand;
+
+/**
+ * Contains tests for
+ * {@code DeleteMiscCommandParser}.
+ */
+public class DeleteMiscCommandParserTest {
+    private final DeleteMiscCommandParser parser = new DeleteMiscCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "2 1", new DeleteMiscCommand(INDEX_SECOND_PERSON, NOTE_FIRST_INDEX));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "2", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteMiscCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArgs_throwsParseException() {
+        assertParseFailure(parser, "   ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteMiscCommand.MESSAGE_USAGE));
+    }
+}
