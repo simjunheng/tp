d601eabafea54b0d37035aa200782dd027a7ab7e
AAlghrairy <abdulelah.faisal@gmail.com>
2022-03-21 04:24:12 +0800

    Fix checkstyle violation

--- src/test/java/seedu/address/logic/commands/ClearTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearTaskCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalIndexes.*;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalTasks.EIGHTH_TASK;
 import static seedu.address.testutil.TypicalTasks.FIFTH_TASK;
