7d034258afe6f786fd37ffef3d73eccc013dfdf8
AAlghrairy <abdulelah.faisal@gmail.com>
2022-03-24 22:14:08 +0800

    Fix checkstyle violations

--- src/test/java/seedu/address/logic/parser/LoadCourtCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/LoadCourtCommandParserTest.java
@@ -1,11 +1,7 @@
 package seedu.address.logic.parser;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-
-import java.io.File;
-import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
