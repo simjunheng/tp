c48246757209aec35ab3f8316318553f93b6bf4f
AAlghrairy <abdulelah.faisal@gmail.com>
2022-03-24 18:52:00 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/logic/commands/LoadCourtCommand.java
+++ src/main/java/seedu/address/logic/commands/LoadCourtCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.io.File;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.image.Image;
