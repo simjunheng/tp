ff818456ba4445e1b5af69c305d90ffc3983def3
juayhee <e0524722@u.nus.edu>
2022-03-27 17:47:24 +0800

    Add classes for implementation of adding a tag to a task

---
+++ src/main/java/seedu/address/logic/commands/AddTagToTaskCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+
+public class AddTagToTaskCommand extends Command {
+    
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+
+}
---
+++ src/main/java/seedu/address/logic/commands/AddTagToTaskCommandParser.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new AddTaskCommand object
+ */
+public class AddTagToTaskCommandParser implements Parser {
+
+    public AddTagToTaskCommand parse(String args) throws ParseException{
+        return null;
+    }
+
+}
