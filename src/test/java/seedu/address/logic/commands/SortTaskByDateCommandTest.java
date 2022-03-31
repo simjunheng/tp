8aa7fc1e8c145ec4e308e1a879505cf24c17ad30
juayhee <e0524722@u.nus.edu>
2022-03-31 17:59:10 +0800

    Add tests for SortTaskByDateCommand

---
+++ src/test/java/seedu/address/logic/commands/SortTaskByDateCommandTest.java
@@ -0,0 +1,100 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalTasks.EIGHTH_TASK;
+import static seedu.address.testutil.TypicalTasks.FIRST_TASK;
+import static seedu.address.testutil.TypicalTasks.NINTH_TASK;
+import static seedu.address.testutil.TypicalTasks.SECOND_TASK;
+import static seedu.address.testutil.TypicalTasks.THIRD_TASK;
+import static seedu.address.testutil.TypicalTasks.getTypicalTaskBook;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.StrategyBoard;
+import seedu.address.model.TaskBook;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+import seedu.address.testutil.PersonBuilder;
+
+class SortTaskByDateCommandTest {
+
+    // Test model
+    private Model model = new ModelManager(getTypicalAddressBook(), getTypicalTaskBook(),
+            new StrategyBoard(), new UserPrefs());
+
+    @Test
+    void execute_sortTaskByDate_success() {
+
+        SortTaskByDateCommand sortTaskByDateCommand = new SortTaskByDateCommand();
+
+        String expectedMessage = String.format(SortTaskByDateCommand.MESSAGE_SORT_TASKS_SUCCESS);
+
+        // Creating expected TaskBook
+        TaskBook expectedTaskBook = new TaskBook();
+        expectedTaskBook.setTasks(Arrays.asList(SECOND_TASK, THIRD_TASK, FIRST_TASK));
+
+        Model expectedModel = new ModelManager(
+                new AddressBook(model.getAddressBook()),
+                new TaskBook(model.getTaskBook()),
+                new StrategyBoard(),
+                new UserPrefs());
+        expectedModel.setTaskBook(expectedTaskBook);
+
+        assertCommandSuccess(sortTaskByDateCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_sortTaskByDateWithSameDay_success() {
+
+        SortTaskByDateCommand sortTaskByDateCommand = new SortTaskByDateCommand();
+
+        String expectedMessage = String.format(SortTaskByDateCommand.MESSAGE_SORT_TASKS_SUCCESS);
+
+        // Test taskbook
+        TaskBook testTaskBook = new TaskBook();
+        testTaskBook.setTasks(Arrays.asList(EIGHTH_TASK, NINTH_TASK));
+
+        TaskBook expectedTaskBook = new TaskBook();
+        expectedTaskBook.setTasks(Arrays.asList(NINTH_TASK, EIGHTH_TASK));
+
+        // Test model
+        Model model = new ModelManager(getTypicalAddressBook(), testTaskBook,
+                new StrategyBoard(), new UserPrefs());
+
+        // Expected model
+        Model expectedModel = new ModelManager(
+                new AddressBook(model.getAddressBook()),
+                new TaskBook(model.getTaskBook()),
+                new StrategyBoard(),
+                new UserPrefs());
+        expectedModel.setTaskBook(expectedTaskBook);
+
+        assertCommandSuccess(sortTaskByDateCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void equals() {
+        final SortTaskByDateCommand command = new SortTaskByDateCommand();
+
+        // If they are the same objects, they are equal
+        assertTrue(command.equals(command));
+
+    }
+}
