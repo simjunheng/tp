b3f0b3b0b4245caf373b7311a0ae0eafc6708f38
Rye-Catcher <2427367956@qq.com>
2022-04-02 09:45:37 +0800

    Fix the special character in player name bug
    Add new constraints to player name and slightly change the parsing workflow of `AddPlayerCommand`.

--- src/main/java/seedu/address/logic/parser/strategy/AddPlayerCommandParser.java
+++ src/main/java/seedu/address/logic/parser/strategy/AddPlayerCommandParser.java
@@ -4,7 +4,9 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.logic.commands.strategy.AddPlayerCommand;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.strategy.Player;
 
 /**
  * Parses input arguments and creates a new AddPlayerCommand object
@@ -19,7 +21,8 @@ public class AddPlayerCommandParser implements Parser<AddPlayerCommand> {
     public AddPlayerCommand parse(String args) throws ParseException {
         try {
             String[] splitArgs = args.trim().split("\\s+", 1);
-            return new AddPlayerCommand(splitArgs[0]);
+            Player player = ParserUtil.paresPlayer(splitArgs[0]);
+            return new AddPlayerCommand(player);
         } catch (IndexOutOfBoundsException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPlayerCommand.MESSAGE_USAGE), pe);
